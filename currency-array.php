bf36ec0e3addbfe6eb78e8fe2b37f0d58410178a
PremSimform <122260605+PremSimform@users.noreply.github.com>
2023-03-20 00:01:51 +0530

    Delete currency-array.php

--- currency-array.php
+++
@@ -1,5 +0,0 @@
-<?php
-$cur = array(
-"USD", "AED", "ARS", "AUD", "BGN", "BRL", "BSD", "BGN", "BRL", "BSD", "CAD", "CHF", "CLP", "CNY", "COP", "CZK", "DKK", "DOP", "EGP", "EUR", "FJD", "GBP", "GTQ", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "ISK", "JPY", "KRW", "KZT", "MVR", "MXN", "MYR", "NOK", "NZD", "PAB", "PEN", "PHP", "PKR", "PLN", "PYG", "RON", "RUB", "SAR", "SEK", "SGD", "THB", "TRY", "TWD", "UAH", "UYU", "ZAR"
-);
-?>
